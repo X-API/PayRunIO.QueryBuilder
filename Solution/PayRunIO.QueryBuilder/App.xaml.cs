ebc0e58a1ce22f1165c5f2dc8937d67422b50fca
CrispinParker <crispinparker@hotmail.com>
2020-11-11 10:14:50 +0000

    -- Added unhandled exception method to App class

--- Solution/PayRunIO.QueryBuilder/App.xaml.cs
+++ Solution/PayRunIO.QueryBuilder/App.xaml.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace PayRunIO.QueryBuilder
+﻿namespace PayRunIO.QueryBuilder
 {
+    using System;
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        /// <summary>
+        /// The application dispatcher unhandled exception method.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The dispatcher unhandled exception event args.</param>
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(
+                $"An unhandled exception just occurred:{Environment.NewLine}{Environment.NewLine}{e.Exception}", 
+                $"Unhandled Exception - {e.Exception.GetType().Name}", 
+                MessageBoxButton.OK, 
+                MessageBoxImage.Error);
+
+            e.Handled = true;
+        }
     }
 }
